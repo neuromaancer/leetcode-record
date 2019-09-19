cad61c8f8c21d291ca1e198c47a0db2ac28c74d3
“AlafateABULIMITI” <felixneoaa@gmail.com>
2019-09-19 16:43:07 +0200

    mergeTwoLists

---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=21 lang=python3
+#
+# [21] Merge Two Sorted Lists
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        temp = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        # If you’re testing two objects using or in Python
+        # then the operator will return the first object that evaluates to true or the last object in the expression
+        cur.next = l1 or l2
+        return temp.next
+
