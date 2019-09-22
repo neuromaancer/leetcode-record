a8da3d2ee491756a4a2f696aaa62bc5e127b23f1
“AlafateABULIMITI” <felixneoaa@gmail.com>
2019-09-22 11:38:43 +0200

    plusOne

---
+++ 66.plus-one.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=66 lang=python3
+#
+# [66] Plus One
+#
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if len(digits) == 1 and digits[0] == 9:
+            return [1, 0]
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+        else:
+            digits[-1] = 0
+            digits[:-1] = self.plusOne(digits[:-1])
+            return digits
+
