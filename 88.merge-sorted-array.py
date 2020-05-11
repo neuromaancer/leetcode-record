e95efd341e02ffb0426f622b690025daf17637e4
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-05-11 12:00:42 +0200

    [88] Merge Sorted Array

---
+++ 88.merge-sorted-array.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+import bisect
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        return nums1.sort()
+
+
+# @lc code=end
