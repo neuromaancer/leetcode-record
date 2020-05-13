03b6cf5a413994eca06faca0d2e1a75e5edef93a
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-05-13 14:41:20 +0200

    [104] Maximum Depth of Binary Tree

---
+++ 104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root:
+            return 1 + max(map(self.maxDepth, (root.left, root.right)))
+        else:
+            return 0
+
+
+# @lc code=end
---
+++ 107.binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=107 lang=python3
+#
+# [107] Binary Tree Level Order Traversal II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        
+# @lc code=end
+
