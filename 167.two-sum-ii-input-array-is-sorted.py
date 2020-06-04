c503088bdd68076cd694aa4c2621d0c8309bf5a4
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-06-04 14:57:53 +0200

    [167] Two Sum II - Input array is sorted

---
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input array is sorted
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        dic = dict()
+        for idx, num in enumerate(numbers):
+            if target - num in dic:
+                return [dic[target - num] + 1, idx + 1]
+            dic[num] = idx
+
+# @lc code=end
