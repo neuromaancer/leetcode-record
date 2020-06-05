f7fcc5e608ebf2da15dcccd65418ebd27d7006b5
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-06-05 20:09:09 +0200

    [633] Sum of Square Numbers

---
+++ 633.sum-of-square-numbers.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=633 lang=python3
+#
+# [633] Sum of Square Numbers
+#
+
+# @lc code=start
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        cc = int(c ** 0.5)
+        left = 0
+        right = cc
+        while left <= right:
+            res = left ** 2 + right ** 2
+            if res == c:
+                return True
+            elif res < c:
+                left += 1
+            elif res > c:
+                right -= 1
+        return False
+
+
+# @lc code=end
