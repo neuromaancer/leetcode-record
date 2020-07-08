899d58c2be920ffefea3ea5a37308eefa0756d0f
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-07-08 11:01:53 +0200

    [524] Longest Word in Dictionary through Deleting

---
+++ 524.longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=524 lang=python3
+#
+# [524] Longest Word in Dictionary through Deleting
+#
+
+# @lc code=start
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        waiting = {}
+        for c in "abcdefghijklmnopqrstuvwxyz":
+            waiting[c] = []
+
+        for word in d:
+            # add pointer to beginning of each word
+            waiting[word[0]].append((word, 0))
+
+        max_len = (0, "")
+        for c in s:
+            words = waiting[c]
+            waiting[c] = []  # clean waiting words for that character
+            for word, idx in words:
+                if idx + 1 >= len(word):
+                    # finished word
+                    # use min and negative length to get maximum length then min word
+                    max_len = min(max_len, (-len(word), word))
+                else:
+                    # move pointer to next word
+                    next_c = word[idx + 1]
+                    waiting[next_c].append((word, idx + 1))
+        return max_len[1]
+
+
+# @lc code=end
+
